d8c4a4bab3cbfbc4fbb1e70cba2cf16b750573f7
Matthew Sherman <msherman0801@gmail.com>
2019-05-30 19:46:50 -0400

    updated content migration datetime error

--- db/migrate/20190426153359_create_contents.rb
+++ db/migrate/20190426153359_create_contents.rb
@@ -5,7 +5,7 @@ class CreateContents < ActiveRecord::Migration[5.2]
       t.text :spouse_two, default: "Spouse Two"
       t.text :about_us, default: "About Us"
       t.text :summary, default: "Summary"
-      t.datetime :date, default: 000
+      t.datetime :date
       t.text :location, default: "Location"
       t.text :contact_name, default: "Contact Name"
       t.text :contact_number, default: "(111) 222-3333"
